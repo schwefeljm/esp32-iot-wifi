b56334db99e0256d7f9eae5a19ce78906d1e4125
Jason Schwefel <jason.schwefel78@gmail.com>
2023-07-13 08:00:54 -0500

    Initial Commit - Build working for WiFi AP, HTTP GET and POST (hello world)

---
+++ main/httpd.c
@@ -0,0 +1,118 @@
+#include "esp_http_server.h"
+#include "esp_spiffs.h"
+#include "esp_vfs.h"
+#include "httpd.h"
+#include "log.h"
+
+
+
+#define INDEX_HTML_PATH "/spiffs/index.html"
+
+char index_html[4096];
+char response_data[4096];
+
+void init_web_page_buffer(void)
+{
+    esp_vfs_spiffs_conf_t conf = {
+        .base_path = "/spiffs",
+        .partition_label = NULL,
+        .max_files = 5,
+        .format_if_mount_failed = false};
+
+    esp_vfs_spiffs_register(&conf);
+
+    memset((void *)index_html, 0, sizeof(index_html));
+    struct stat st;
+    if (stat(INDEX_HTML_PATH, &st))
+    {
+        ESP_LOGE(TAG, "index.html not found");
+        return;
+    }
+
+    FILE *fp = fopen(INDEX_HTML_PATH, "r");
+    if (fread(index_html, st.st_size, 1, fp) == 0)
+    {
+        ESP_LOGE(TAG, "fread failed");
+    }
+    fclose(fp);
+}
+
+esp_err_t send_web_page(httpd_req_t *req)
+{
+    int response;
+
+    //char* WIFI_SSID = nvs_load_value_if_exist("wifi-ssid");
+    sprintf(response_data, index_html, "WIFI_SSID", "WIFI_PASS");
+    response = httpd_resp_send(req, response_data, HTTPD_RESP_USE_STRLEN);
+    return response;
+}
+
+
+esp_err_t get_handler(httpd_req_t *req)
+{
+    return send_web_page(req);
+}
+
+
+esp_err_t post_handler(httpd_req_t *req)
+{
+    /* Destination buffer for content of HTTP POST request.
+     * httpd_req_recv() accepts char* only, but content could
+     * as well be any binary data (needs type casting).
+     * In case of string data, null termination will be absent, and
+     * content length would give length of string */
+    char content[100];
+
+    /* Truncate if content length larger than the buffer */
+    size_t recv_size = MIN(req->content_len, sizeof(content));
+
+    int ret = httpd_req_recv(req, content, recv_size);
+    if (ret <= 0) {  /* 0 return value indicates connection closed */
+        /* Check if timeout occurred */
+        if (ret == HTTPD_SOCK_ERR_TIMEOUT) {
+            /* In case of timeout one can choose to retry calling
+             * httpd_req_recv(), but to keep it simple, here we
+             * respond with an HTTP 408 (Request Timeout) error */
+            httpd_resp_send_408(req);
+        }
+        /* In case of error, returning ESP_FAIL will
+         * ensure that the underlying socket is closed */
+        return ESP_FAIL;
+    }
+
+    /* Send a simple response */
+    const char resp[] = "URI POST Response";
+    httpd_resp_send(req, resp, HTTPD_RESP_USE_STRLEN);
+    return ESP_OK;
+}
+
+httpd_uri_t uri_get = {
+    .uri = "/",
+    .method = HTTP_GET,
+    .handler = get_handler,
+    .user_ctx = NULL};
+
+httpd_uri_t uri_post = {
+    .uri = "/",
+    .method = HTTP_POST,
+    .handler = post_handler,
+    .user_ctx = NULL};
+
+
+
+
+httpd_handle_t setup_http_server(void)
+{
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+    httpd_handle_t server = NULL;
+
+    if (httpd_start(&server, &config) == ESP_OK)
+    {
+        httpd_register_uri_handler(server, &uri_get);
+        httpd_register_uri_handler(server, &uri_post);
+        
+    }
+
+    return server;
+}
+
---
+++ main/wifi.c
@@ -0,0 +1,112 @@
+#include <string.h>
+#include "esp_mac.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "nvs.h"
+#include "log.h"
+
+#include "wifi.h"
+#include "globals.h"
+
+
+
+bool init_wifi(bool override)
+{
+
+    iot_configuration_t.wifi_settings_t = get_wifi_settings_from_nvs();
+    switch (iot_configuration_t.wifi_settings_t.mode) 
+    {
+        case -1:
+        //Put panic here
+        break;
+
+        //Factory Default in AP Mode
+        case 1:
+        wifi_init_softap(iot_configuration_t.wifi_settings_t);
+
+    }
+
+    return true;
+}
+
+
+wifi_connection_s get_wifi_settings_from_nvs()
+{
+    struct wifi_connection_s settings;
+    settings.mode = nvs_load_int_value_if_exist("wifi-mode");
+    settings.ssid = nvs_load_str_value_if_exist("wifi-ssid");
+    settings.passwd = nvs_load_str_value_if_exist("wifi-passwd"); 
+
+    return settings;
+}
+
+
+
+//static const char *TAG = "Jason's App";
+
+//wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+
+static void wifi_event_handler(void* arg, esp_event_base_t event_base,
+                                    int32_t event_id, void* event_data)
+{
+  if (event_id == WIFI_EVENT_AP_STACONNECTED) {
+      wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
+      ESP_LOGI(TAG, "station "MACSTR" join, AID=%d",
+                MAC2STR(event->mac), event->aid);
+  } else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
+      wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
+      ESP_LOGI(TAG, "station "MACSTR" leave, AID=%d",
+                MAC2STR(event->mac), event->aid);
+
+  }
+}
+
+
+void wifi_init_softap(wifi_connection_s settings)
+    {
+    ESP_LOGI(TAG, "WiFi Start");
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_ap();
+
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &wifi_event_handler,
+                                                        NULL,
+                                                        NULL));
+
+    ESP_LOGI(TAG, "esp_event_handler_instance_register Complete");
+    wifi_config_t wifi_config = {
+        .ap = {
+            .channel = 5,
+            .max_connection = 1,
+            .authmode = WIFI_AUTH_WPA2_PSK,
+            .pmf_cfg = {
+                    .required = true,
+            },
+        },
+  };
+
+    strncpy((char*)wifi_config.ap.ssid, (char*)settings.ssid, 32);
+    strncpy((char*)wifi_config.ap.password, (char*)settings.passwd, 64);
+  
+  
+
+    ESP_LOGI(TAG, "AP Configuration Complete.");
+
+    ESP_LOGI(TAG, "Mode set to AP.");
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+    ESP_LOGI(TAG, "Enabling WIFI.");
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config));
+    ESP_LOGI(TAG, "Starting AP.");
+    ESP_LOGI(TAG, "wifi_init_softap finished. SSID:%s password:%s", settings.ssid, settings.passwd);
+
+  
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+  
+}
+
